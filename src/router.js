1be7261bec22dea6f3e0e5431a1c593c4dc89f50
Diego <idfernando3@gmail.com>
2022-02-17 22:10:18 -0500

    Stats view added

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
+import Stats from './views/Stats.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/stats',
+    name: 'Stats',
+    component: Stats
   },
 ]
 
